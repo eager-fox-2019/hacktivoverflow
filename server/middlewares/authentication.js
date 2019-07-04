96968137bc282c5d0c173c751cada6a1c2e10065
aryadimade <madearyadi@gmail.com>
2019-07-04 17:37:17 +0700

    fix catch syntax error

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
       const decode = decodeToken(req.headers.access_token);
       req.decode = decode;
       next();
-    } catch {
+    } catch(e){
       next({
         code: 401
       })
