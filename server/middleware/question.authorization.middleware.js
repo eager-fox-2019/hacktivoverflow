dc7e9f0114ba31a3e13b172665c71484121cba4c
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-04 11:55:11 +0700

    update question authorization middleware

--- server/middleware/question.authorization.middleware.js
+++ server/middleware/question.authorization.middleware.js
@@ -5,7 +5,7 @@ module.exports = async function (req, res, next) {
   let user = req.user
   let question = await Question.findOne({_id: id}).exec()
   if (question) {
-    if (question.user === user) {
+    if (question.user == user) {
       next()
     } else {
       next ({code:401, msg: "not authorized"})
