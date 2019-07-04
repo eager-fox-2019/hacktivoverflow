5708d70839309b31ffbc6b82ecf3b3b1ac5fb798
Melina Dewi <hds.melina@gmail.com>
2019-07-04 14:02:58 +0700

    Adding documentation

--- server/routes/answer.js
+++ server/routes/answer.js
@@ -3,11 +3,11 @@ const answerController = require('../controllers/answer')
 const { authentication, authorizationAnswer } = require('../middlewares/auth')
 
 router.get('/:answerId', answerController.getAnswer)
-router.get('/:answerId', answerController.getAllAnswer) //for test purpose only
+// router.get('/', answerController.getAllAnswer) //for test purpose only
 router.use(authentication)
 router.post('/', answerController.addAnswer)
 router.patch('/update-detail/:answerId', answerController.likesUpdate)
-router.get('/:userId', answerController.getAnswer)
+// router.get('/:userId', answerController.getUserAnswer) // next modification
 router.patch('/:answerId', authorizationAnswer, answerController.update)
 router.delete('/:answerId', authorizationAnswer, answerController.delete)
 
