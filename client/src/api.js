dc1a548400126f4adb253a84094a4e78e6581749
rizky <andijani21.rizky@gmail.com>
2019-07-05 13:25:00 +0700

    cek2

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const ax = axios.create({
-    baseURL : 'http://18.221.211.144:3000'
+    baseURL : 'http://18.221.211.144'
 })
 
 module.exports = ax
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 const User = require('../models/user')
 const { compare } = require('../helpers/bcrypt')
 const { sign } = require('../helpers/jwt')
-const { nodeMailer } = require('../helpers/nodeMailer')
+// const { nodeMailer } = require('../helpers/nodeMailer')
 
 class UserController{
     static login(req,res,next){
@@ -46,7 +46,7 @@ class UserController{
         newUser.save()
         .then(user =>{
             console.log('registered', user);
-            nodeMailer(user.email,'welcome')
+            // nodeMailer(user.email,'welcome')
             res.status(201).json(user)
         })
         .catch(next)
